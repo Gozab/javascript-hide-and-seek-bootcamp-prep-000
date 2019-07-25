df5ce7a1c05233f36f0e110a1cd45c30444681f0
Benjamin Goza <bengoza@gmail.com>
2019-07-25 19:29:39 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
+function getFirstSelector(selector) {
+    return document.querySelector(selector);
+	//return d;
+}
 
+
+
+
+function nestedTarget(){
+  return document.querySelector('#nested .target')
+}
+
+
+function increaseRankBy(n) {
+  const lis = document
+  .getElementById('app')
+  .querySelectorAll('ul.ranked-list li');
+
+  for (let i = 0; i < lis.length; i++) {
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+}
+      
+}
+
+
+function deepestChild() {
+  const picard = document.getElementById('grand-node').querySelectorAll('div');
+    var last = picard.length -1;
+        return picard[last];
+  
+}
